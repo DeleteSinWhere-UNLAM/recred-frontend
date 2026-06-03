9442271406c70a467cd502833ad6613a4ffafa78
AlexisCub <alcubillabenitez@alumno.unlam.edu.ar>
2026-06-03 13:45:03 -0300

    fix: preferencias service con inject y pipe map

--- src/app/features/preferencias/services/preferencias.service.ts
+++ src/app/features/preferencias/services/preferencias.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Preferencia } from '../models/preferencia.model';
 
 @Injectable({ providedIn: 'root' })
 export class PreferenciasService {
+  private readonly http = inject(HttpClient);
+
   private readonly baseUrl = 'https://18-119-187-167.sslip.io';
 
   private readonly alumnoId = '7058aa34-c843-41ca-a8dc-27c496fa7413';
-
-  constructor(private readonly http: HttpClient) {}
 
   getPreferencias(): Observable<Preferencia[]> {
     return this.http
