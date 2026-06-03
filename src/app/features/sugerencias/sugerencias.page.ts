cfc400c6ef5d6d20053372296384569ef905be8d
AlexisCub <alcubillabenitez@alumno.unlam.edu.ar>
2026-06-03 05:15:30 -0300

    fix: integración de sugerencias con backend

--- src/app/features/sugerencias/sugerencias.page.ts
+++ src/app/features/sugerencias/sugerencias.page.ts
@@ -2,38 +2,38 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
-
 import { UsuarioService } from '../../data-access/services/usuario.service';
-import { SugerenciasService } from './services/sugerencias.service';
+import { SugerenciasService } from '../../features/sugerencias/services/sugerencias.service';
 
-import { SugerenciaProducto } from './models/sugerencia-producto.model';
+import { SugerenciaProducto } from '../../features/sugerencias/models/sugerencia-producto.model';
 
 import { SugerenciaCardComponent } from './components/sugerencia-card/sugerencia-card.component';
 
 @Component({
   selector: 'app-sugerencias-page',
+  standalone: true,
   templateUrl: './sugerencias.page.html',
   styleUrl: './sugerencias.page.css',
-  imports: [NavbarComponent, SugerenciaCardComponent]
+  imports: [NavbarComponent, SugerenciaCardComponent],
 })
 export class SugerenciasPage {
-
-  private readonly usuarioService =
-    inject(UsuarioService);
-
-  private readonly sugerenciasService =
-    inject(SugerenciasService);
-
+  private readonly usuarioService = inject(UsuarioService);
+  private readonly sugerenciasService = inject(SugerenciasService);
   private readonly router = inject(Router);
 
-  readonly nombreUsuario =
-    this.usuarioService.getUsuarioActual().nombre;
+  readonly nombreUsuario = this.usuarioService.getUsuarioActual().nombre;
 
-  readonly sugerencias: SugerenciaProducto[] =
-    this.sugerenciasService.getSugerencias();
+  readonly usuarioId = this.usuarioService.getUsuarioActual().id;
+
+  sugerencias: SugerenciaProducto[] = [];
 
   constructor() {
     this.usuarioService.setHomeUrl('/kiosquero');
+
+    this.sugerenciasService.getSugerencias(this.usuarioId).subscribe((data) => {
+      this.sugerencias = data;
+      console.log('SUGERENCIAS:', data);
+    });
   }
 
   volver(): void {
