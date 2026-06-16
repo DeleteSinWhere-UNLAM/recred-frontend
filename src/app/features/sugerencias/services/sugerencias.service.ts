169780fee97e2869936a6e374f1d603d217a61a9
MRRegner <rodriguez.regner@gmail.com>
2026-06-15 21:09:30 -0300

    Modificado endpoint para obtener listado de productos menos vendidos

--- src/app/features/sugerencias/services/sugerencias.service.ts
+++ src/app/features/sugerencias/services/sugerencias.service.ts
@@ -1,31 +1,46 @@
 import { Injectable, inject } from '@angular/core';
+
 import { HttpClient } from '@angular/common/http';
-
-import { Observable, map } from 'rxjs';
-
+ 
+import { Observable } from 'rxjs';
+ 
 import { environment } from '../../../../environments/environment';
 
 import { SugerenciaProducto } from '../models/sugerencia-producto.model';
-
+ 
 @Injectable({
+
   providedIn: 'root',
+
 })
+
 export class SugerenciasService {
+
   private readonly http = inject(HttpClient);
 
   private readonly baseUrl = environment.apiUrl;
-
+ 
   getSugerencias(usuarioId: string): Observable<SugerenciaProducto[]> {
-    return this.http
-      .get<
-        SugerenciaProducto[]
-      >(`${this.baseUrl}/kiosqueros/${usuarioId}/sugerencia-cambio-producto`)
-      .pipe(map((response) => response));
+
+    return this.http.get<SugerenciaProducto[]>(
+
+      `${this.baseUrl}/kiosqueros/${usuarioId}/lista-sugerencia-cambio-producto`
+
+    );
+
+  }
+ 
+  comprarSugerencia(sugerenciaId: string): Observable<void> {
+
+    return this.http.post<void>(
+
+      `${this.baseUrl}/sugerencias-consumo/comprar`,
+
+      { sugerenciaId }
+
+    );
+
   }
 
-  comprarSugerencia(sugerenciaId: string): Observable<void> {
-    return this.http.post<void>(`${this.baseUrl}/sugerencias-consumo/comprar`, {
-      sugerenciaId,
-    });
-  }
 }
+ 
